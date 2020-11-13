5d52991c0b921c744caa9ec0e6303ef082b1f1eb
Desoppere <59663882+Desoppere@users.noreply.github.com>
2020-11-13 11:10:55 +0100

    tijdelijk opslag

--- main.ts
+++ main.ts
@@ -1,3 +1,72 @@
+let KeuzeBSS2 = 0
+let WaardeBSS = 0
+function SpelerBSS () {
+    while (!(input.buttonIsPressed(Button.A)) && !(input.buttonIsPressed(Button.B) && !(input.buttonIsPressed(Button.AB)))) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . . . # .
+            . . # . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (input.buttonIsPressed(Button.A)) {
+        KeuzeBSS2 = 0
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        KeuzeBSS2 = 1
+    }
+    if (input.buttonIsPressed(Button.AB)) {
+        KeuzeBSS2 = 2
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    KeuzeBSS()
+    basic.showNumber(WaardeBSS)
+})
+// Laat de microbit zijn Blad Steen of Schaar kiezen.0=Blad 1=Steen 2=Schaar
+function KeuzeBSS () {
+    WaardeBSS = randint(0, 2)
+}
+function Aftel () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+}
 basic.forever(function () {
-	
+    KeuzeBSS()
+    Aftel()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    SpelerBSS()
 })
